5a7a69229801008f236112bec766bd09c540d5fe
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-09-16 14:28:53 -0300

    Exercicio 1 realizado

---
+++ src/exercise1.ts
@@ -0,0 +1,7 @@
+for(let i :number = 1; i <= 10; i++){
+    console.log(`Tabuada do ${i}`)
+    for (let j: number = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i*j}`)
+    }
+    console.log("-------------------")
+}
