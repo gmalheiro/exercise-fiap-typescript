9224732112a44b9cf9f125176d96f72609352db1
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-09-16 15:08:52 -0300

    Exercicio 2 feito

--- dist/exercise1.js
+++ dist/exercise1.js
@@ -1,8 +1,8 @@
 "use strict";
 for (let i = 1; i <= 10; i++) {
-    console.log(`Tabuada do ${i}`);
+    console.log(`Tabuada do ${i}:`);
     for (let j = 1; j <= 10; j++) {
         console.log(`${i} x ${j} = ${i * j}`);
     }
-    console.log("-------------------");
+    console.log("------------------");
 }
